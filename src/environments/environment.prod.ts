ff1d596ed4688b8e6040a213b76f824200c274b8
Lk6Eng05 <bdkusuma@gmail.com>
2018-03-24 17:00:43 +0700

    update base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
-export const environment = {
-  production: true,
-  baseUrl:'http://localhost:8000',
-  v1:'api/v1/',
-  v2:'api/v2/'
-};
-
 // export const environment = {
 //   production: true,
-//   baseUrl:'http://tkt.laku6.com:44180',
+//   baseUrl:'http://localhost:8000/',
 //   v1:'api/v1/',
 //   v2:'api/v2/'
 // };
+
+export const environment = {
+  production: true,
+  baseUrl:'http://tkt.laku6.com:44180/',
+  v1:'api/v1/',
+  v2:'api/v2/'
+};
