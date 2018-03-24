fb1107b0043e299336ddf29a35d105c75cf1adf2
Lk6Eng05 <bdkusuma@gmail.com>
2018-03-24 18:01:51 +0700

    update base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@
 export const environment = {
   production: true,
   baseUrl:'//tkt.laku6.com:44180/',
-  deploy:'//cygnustales.github.io/mobile/',
+  deploy:'/',
   v1:'api/v1/',
   v2:'api/v2/'
 };
